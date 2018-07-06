3825de7c36397802ff732edb30820f1ba40a3481
Tim Kilian <mail@timkilian.pl>
2018-07-06 11:14:09 +0200

    Add service worker, dummy logo

---
+++ public/worker.js
@@ -0,0 +1,49 @@
+const CACHE_ID = "news-duell-v1";
+
+const FILES = [
+  "./",
+  "js/app.js",
+  "css/app.css",
+  "lib/localforage.min.js",
+  "assets/icons/icon-72x72.png",
+  "assets/icons/icon-96x96.png",
+  "assets/icons/icon-128x128.png",
+  "assets/icons/icon-144x144.png",
+  "assets/icons/icon-152x152.png",
+  "assets/icons/icon-192x192.png",
+  "assets/icons/icon-384x384.png",
+  "assets/icons/icon-512x512.png",
+  "favicon.ico",
+  "manifest.json",
+];
+
+
+
+
+const handleInstallation = async event => {
+  console.log(`Installing version ${CACHE_ID}`);
+
+  const cache = await caches.open(CACHE_ID);
+  return await cache.addAll(FILES);
+};
+
+self.addEventListener("install", event => event.waitUntil(handleInstallation(event)) );
+
+
+const handleActivation = async () => {
+  console.log(`Activating version ${CACHE_ID}`);
+
+  const claim = self.clients.claim();
+  const cacheKeys = await self.caches.keys();
+  const toDelete = cacheKeys.filter(key => key !== CACHE_ID);
+  return Promise.all([ claim, ...toDelete.map(key => self.caches.delete(key))]);
+};
+
+self.addEventListener("activate", event => event.waitUntil(handleActivation()) );
+
+const updateCache = async (request, response) => {
+  const clone = response.clone();
+  const cache = await caches.open(CACHE_ID);
+  return await cache.put(request, clone);
+};
+
