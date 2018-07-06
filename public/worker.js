92bc395fd42304b2fd1fe51fe9a896bde5d4bd03
Tim Kilian <mail@timkilian.pl>
2018-07-06 12:07:52 +0200

    Fetch from cache

--- public/worker.js
+++ public/worker.js
@@ -1,24 +1,21 @@
-const CACHE_ID = "news-duell-v1";
+const CACHE_ID = 'news-duell-v1';
 
 const FILES = [
-  "./",
-  "js/app.js",
-  "css/app.css",
-  "lib/localforage.min.js",
-  "assets/icons/icon-72x72.png",
-  "assets/icons/icon-96x96.png",
-  "assets/icons/icon-128x128.png",
-  "assets/icons/icon-144x144.png",
-  "assets/icons/icon-152x152.png",
-  "assets/icons/icon-192x192.png",
-  "assets/icons/icon-384x384.png",
-  "assets/icons/icon-512x512.png",
-  "favicon.ico",
-  "manifest.json",
+  './',
+  'js/app.js',
+  'css/app.css',
+  'lib/localforage.min.js',
+  'assets/icons/icon-72x72.png',
+  'assets/icons/icon-96x96.png',
+  'assets/icons/icon-128x128.png',
+  'assets/icons/icon-144x144.png',
+  'assets/icons/icon-152x152.png',
+  'assets/icons/icon-192x192.png',
+  'assets/icons/icon-384x384.png',
+  'assets/icons/icon-512x512.png',
+  'favicon.ico',
+  'manifest.json',
 ];
-
-
-
 
 const handleInstallation = async event => {
   console.log(`Installing version ${CACHE_ID}`);
@@ -27,7 +24,7 @@ const handleInstallation = async event => {
   return await cache.addAll(FILES);
 };
 
-self.addEventListener("install", event => event.waitUntil(handleInstallation(event)) );
+self.addEventListener('install', event => event.waitUntil(handleInstallation(event)));
 
 
 const handleActivation = async () => {
@@ -36,10 +33,10 @@ const handleActivation = async () => {
   const claim = self.clients.claim();
   const cacheKeys = await self.caches.keys();
   const toDelete = cacheKeys.filter(key => key !== CACHE_ID);
-  return Promise.all([ claim, ...toDelete.map(key => self.caches.delete(key))]);
+  return Promise.all([claim, ...toDelete.map(key => self.caches.delete(key))]);
 };
 
-self.addEventListener("activate", event => event.waitUntil(handleActivation()) );
+self.addEventListener('activate', event => event.waitUntil(handleActivation()));
 
 const updateCache = async (request, response) => {
   const clone = response.clone();
@@ -47,3 +44,14 @@ const updateCache = async (request, response) => {
   return await cache.put(request, clone);
 };
 
+const handleFetch = async event => {
+  console.log('Load from cache: ' + event.request);
+
+  const response = await caches.match(event.request);
+  if (response) {
+    return response;
+  }
+  return fetch(event.request).then(async response => updateCache(event.request, response));
+};
+
+self.addEventListener('fetch', event => event.respondWith(handleFetch(event)));
