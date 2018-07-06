d89e4b7936aa1ea0254cf87b801bcba9faf10a32
Tim Kilian <mail@timkilian.pl>
2018-07-06 13:29:21 +0200

    Add routes for api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    Route::get('questions/random', function () {
+        return 'Random post';
+    });
+    Route::get('questions/{question}', function (App\Question $question) {
+        return $question;
+    });
+    Route::get('answers/{answer}', function (App\Answer $answer) {
+        return $answer;
+    });
+    Route::post('questions/create', function () {
+        return 'Question created';
+    });
+});
